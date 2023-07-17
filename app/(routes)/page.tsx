cce8958042a24c288233bf73ec0b5270ec6a392f
Aditya <adi2652002@gmail.com>
2023-07-17 12:49:42 +0530

    Billboard string Updated

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("4b5100ce-6929-438b-9f2d-f5adcbe0e05a");
 
   return (
     <Container>
